35d5b770372f44cb11936f95a0e353322676e6da
NG-Corp <91930641+NG-Corp@users.noreply.github.com>
2022-08-04 14:12:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = "https://api.github.com/rest1;
+const PORT = "https://api.github.com/rest1";
 const res = require("res");
 const req = require("request");
 const { createWorker} = require("tesseract.js");
