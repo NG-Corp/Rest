520740c3ce2a15049f45913a31705cc43b942039
NG-Corp <91930641+NG-Corp@users.noreply.github.com>
2022-08-04 11:12:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const res = require("res");
+const req = require("request");
+const { createWorker} = require("tesseract.js");
+const { text } = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const worker = createWorker();
+
+app.use( express.json() )
+app.use(cors({origin: true, credentials: true}));
+
+app.get('/tes', (req,res) => {
+
+    res.status(200).send({
+        'tshirt':"ani",
+    })
+}) 
+
+app.post('/tio', (req, res, err) => {
+
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({ message: "We Need A Logo"})
+    }
+
+    
+        async function nea() {
+             await worker.load();
+             await worker.loadLanguage('eng');
+             await worker.initialize('eng');
+             const text  = await worker.recognize(logo);
+             var ret = text.data.text.replace('\n',' ');
+             await console.log(ret);
+             
+             await res.send({
+                "data":`${ret}`
+             });
+             await worker.terminate();
+     } 
+
+     }
+
+);
+app.listen(
+    PORT,
+    () => console.log(`open localhost`)
+)
+
